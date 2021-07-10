cb3c4ebc0714f4556fe0428ebf685afa669efcaa
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-09 21:03:32 -0300

    code 1 e 2

---
+++ codes/processos/2_execv.c
@@ -0,0 +1,27 @@
+// codigo do livro Sistemas Distribuidos
+// pag. 56
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main ()
+{
+    char *cmd[] = { "cat" , "/etc/passwd" , (char *)0 };
+
+    printf("Vou chamar o programa cat para ler o conteúdo de /etc/passwd. \n");
+
+    execv("/bin/cat", cmd);
+
+    printf("Esta mensagem não será impressa. \n");
+    
+    exit(0);
+}
+
+/*
+Para compilar(no LINUX) este programa como "execv" :
+# gcc 2_execv.c -o execv
+
+Para rodar:
+# ./execv
+
+*/
