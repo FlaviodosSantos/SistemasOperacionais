8c0944d54332d95f4fb39540a81a3752ce678488
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-09 21:03:01 -0300

    segundo commit

---
+++ codes/processos/1_meupid.c
@@ -0,0 +1,22 @@
+// codigo do livro Sistemas Distribuidos
+// pag. 55
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main ()
+{
+    printf("Eu sou o processo %d. Meu pai é o processo %d. 
+        O ID do grupo de processo é o %d. \n" , getpid(), getppid(), getpgrp());
+
+    exit(0);
+}
+
+/*
+Para compilar(no LINUX) este programa como "meupid" :
+# gcc 1_meupid.c -o meupid
+
+Para rodar:
+# ./meupid
+
+*/
