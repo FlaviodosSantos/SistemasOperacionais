1048aeff4c9b073a9f4261a87bde15d208e03cd5
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-07-09 22:24:37 -0300

    add 1_exemplo-pthread

---
+++ codes/threads/1_exemplo_pthread.c
@@ -0,0 +1,35 @@
+// codigo do livro Sistemas Distribuidos
+// pag. 55
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* funcao_thread(void* arg)
+{
+    printf("Nova thread criada com o PID %d. \n", getpid());
+    sleep(20);
+    return NULL;
+}
+
+int main ()
+{
+    pthread_t thread;
+    printf("Processo pesado PID %d. \n", getpid());
+
+    pthread_create (&thread , NULL, &funcao_thread, NULL);
+    printf("Indentificador do thread %ld. \n", thread);
+
+    sleep(20);
+    exit(0);
+}
+
+/*
+Para compilar(no LINUX) este programa como "exemplo_pthread" :
+# gcc -pthread 1_exemplo_pthread.c -o exemplo_pthread
+
+Para rodar:
+# ./exemplo_pthread
+
+*/
