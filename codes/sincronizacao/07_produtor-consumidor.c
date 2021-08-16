e72d834494aea0cadbb6b54acfe9ebe92f30576c
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-08-15 23:54:45 -0300

    mod parte do exemplo produtor-consumidor

--- codes/sincronizacao/07_produtor-consumidor.c
+++ codes/sincronizacao/07_produtor-consumidor.c
@@ -1,3 +1,6 @@
+// livro Sistemas Operacionais Modernos - Tanenbaum
+// exemplo da pagina 95
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -6,3 +9,23 @@
 pthread_mutex_t the_mutex;
 pthread_cond_t condc, condp;
 int buffer = 0;
+
+void *producer(void *ptr)
+{
+    int i;
+
+    for (i = 1; i <= MAX; i++){
+        pthread_mutex_lock(&the_mutex);
+        
+        while (buffer != 0) {
+            pthread_cond_wait(&condp, &the_mutex);
+        }
+        
+        buffer = i;
+
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&the_mutex);
+    }
+
+    pthread_exit(0);
+}
